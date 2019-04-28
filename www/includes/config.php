9f69c4c80bbd7163438efce3c380fc97eb43ccd6
Rajesh Medampudi <rajesh@simbotix.com>
2019-04-28 11:15:16 +0530

    major config.php

--- www/includes/config.php
+++ www/includes/config.php
@@ -4,14 +4,16 @@
 	//----------------------------------------------------------------------------------//
 	
 	/*  Set the URL to your Sendy installation (without the trailing slash) */
-	define('APP_PATH', 'http://your_sendy_installation_url');
+	define('APP_PATH', getenv('SENDY_PATH'));
 	
 	/*  MySQL database connection credentials (please place values between the apostrophes) */
-	$dbHost = ''; //MySQL Hostname
-	$dbUser = ''; //MySQL Username
-	$dbPass = ''; //MySQL Password
-	$dbName = ''; //MySQL Database Name
-	
+	$dbHost = 'mysql'; //MySQL Hostname
+	$dbUser = 'root'; //MySQL Username
+	$dbPass = getenv('MYSQL_ROOT_PASSWORD'); //MySQL Password
+	$dbName = getenv('MYSQL_DATABASE'); //MySQL Database Name
+
+	$environment = getenv('SENDY_ENV');
+	$testEmail = getenv('SENDY_LOCAL_EMAIL_TO');
 	
 	//----------------------------------------------------------------------------------//	
 	//								  OPTIONAL SETTINGS
@@ -24,7 +26,7 @@
 	$charset = 'utf8mb4';
 	
 	/*  Set this if you use a non standard MySQL port.  */
-	$dbPort = 3306;	
+	$dbPort = getenv('MYSQL_PORT');	
 	
 	/*  Domain of cookie (99.99% chance you don't need to edit this at all)  */
 	define('COOKIE_DOMAIN', '');
